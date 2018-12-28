78f8a2fcfc2f7c959bd2e1c78773eacb8880aa0d
DarbyORear <darbyorear@gmail.com>
2018-12-27 22:33:21 -0600

    displays cart in grid form. added part of delete feature.

--- script.js
+++ script.js
@@ -1,63 +1,135 @@
+
+var chokerPrice = 25;
+var redNecklacePrice = 30;
+var mookitePrice = 20;
+var ankletPrice = 15;
+var gflNecklacePrice = 18;
+var greenAndPurplePrice = 40;
+
 var arrayI = []; //arrayItemsPurchased;
 var arrayP = []; //arrayPurchasePrices;
 var display = document.getElementById("display");
 var price = document.getElementsByClassName("itemprice");
+// var testThis = document.getElementById("testThis");
+// var removeChoker = document.getElementById("chokerRemove");
+// var totalSum = document.getElementById("totalSum");
+var x;
 
+function removeFromCart(){
+//1. delete row  2. delete item from cart array  3. change total sum
+document.getElementById("htmlTable").deleteRow(this);
+alert("item removed");
+//NEED TO UPDATE TOTAL PRICE WHEN AN ITEM IS REMOVED.
+// document.getElementById("totalSum");
+// totalSum.innerHTML = "$0";
+//1. DELETE ITEM FROM ARRAY:
+//how to do this:
 
-function addNecklaceToCart(){
-
-  var a = document.getElementById("necklaceItem");
-  var b = document.getElementById("necklacePrice");
-  arrayI.push(a.innerHTML);
-  arrayP.push(b.innerHTML);										
-  alert(arrayI + ", " + arrayP);
 }
 
 
-function addBraceletToCart(){
-  var a = document.getElementById("braceletItem");
-  var b = document.getElementById("braceletPrice");
+
+/*old function:
+function addChokerToCart(){
+
+  var a = document.getElementById("chokerItem");
+  var b = document.getElementById("chokerPrice");
+  arrayI.push(a.innerHTML);
+  arrayP.push(b.innerHTML);										
+  alert(arrayI + ", " + arrayP);
+}*/
+
+//new function:
+function addChokerToCart(){
+
+  var a = document.getElementById("chokerItem");
+  arrayI.push(a.innerHTML);
+  arrayP.push(chokerPrice);                   
+  alert(arrayI + ", " + arrayP);
+}
+
+/*old function:
+function addRedNecklaceToCart(){
+  var a = document.getElementById("redNecklaceItem");
+  var b = document.getElementById("redNecklacePrice");
   arrayI.push(a.innerHTML);
   arrayP.push(b.innerHTML);
   alert(arrayI + ", " + arrayP);
-  // var sum = arrayP[0] + arrayP[1];   //HOW DO I ADD THE PRICES TOEGETHER???
-  // alert(sum);
-}								
+}*/
 
-
-function addEarringsToCart(){
-  var a = document.getElementById("earringsItem");
-  var b = document.getElementById("earringsPrice");
+//new function:
+function addRedNecklaceToCart(){
+  var a = document.getElementById("redNecklaceItem");
   arrayI.push(a.innerHTML);
-  arrayP.push(b.innerHTML);
+  arrayP.push(redNecklacePrice);
+  alert(arrayI + ", " + arrayP);
+}					
+
+
+function addMookiteToCart(){
+  var a = document.getElementById("mookiteItem");
+  arrayI.push(a.innerHTML);
+  arrayP.push(mookitePrice);
   alert(arrayI + ", " + arrayP);
 }				
 
 
 function addAnkletToCart(){
   var a = document.getElementById("ankletItem");
-  var b = document.getElementById("ankletPrice");
   arrayI.push(a.innerHTML);
-  arrayP.push(b.innerHTML);
+  arrayP.push(ankletPrice);
   alert(arrayI + ", " + arrayP);
 }						
 
 
-function addToeRingToCart(){
-  var a = document.getElementById("toeRingItem");
-  var b = document.getElementById("toeRingPrice");
+function addGFLNecklaceToCart(){
+  var a = document.getElementById("gflNecklaceItem");
   arrayI.push(a.innerHTML);
-  arrayP.push(b.innerHTML);
+  arrayP.push(gflNecklacePrice);
   alert(arrayI + ", " + arrayP);					
+}
+
+function addGreenAndPurpleToCart() {
+  var a = document.getElementById("greenAndPurpleItem");
+  arrayI.push(a.innerHTML);
+  arrayP.push(greenAndPurplePrice);
+  alert(arrayI + ", " + arrayP);
 }
 
 
 function sumPrices(){
-//add prices together and give user the total.
-//prices should be in nums so we can add them. then present as string answer.
-parsedPrice = parseFloat(price);
-  alert(parsedPrice);
+  var sumTotal = 0;
+  // var arrayTotal = [];
+  for(var i = 0; i < arrayI.length; i++) {
+    // arrayTotal.push(arrayP[i]);
+    sumTotal = sumTotal + arrayP[i];
+  }
+return sumTotal;
+
 }
+
+function newSum(){
+  //if at least one delete button has been clicked, execute function.
+  //else, sumPrices(). ????
+
+  //this function is for when a row is deleted. Need to update the total...
+  //which means the whole array needs to be modified.
+  var sumTotal = 0;
+  // var arrayTotal = [];
+  for(var i = 0; i < arrayI.length; i++) {
+    // arrayTotal.push(arrayP[i]);
+    sumTotal = sumTotal - arrayP[i];
+  }
+return sumTotal;
+
+}
+
+
+
+function yesOk(){
+  alert("Yay! Congrats on your purchases.");
+}
+
 
 
 function displayShoppingCart(){
@@ -65,12 +137,58 @@ function displayShoppingCart(){
   var newArray = []
   for(var i = 0; i < arrayI.length; i++){
     newArray.push(" " + arrayI[i]);
-    newArray.push(" " + arrayP[i]);
+    newArray.push(" " + "$" + arrayP[i]);
   }
 
-  display.innerHTML = ("<p>Here are the items you are purchasing: </p>" + newArray + "<p>Amount OK?" + "<button>Yes</button>" + "  " + "<button>No</button></p>");
+
+
+// function deleteRow(r){ //FIX ME - THIS STILL DOESN'T WORK
+//   //delete row
+//     var i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("htmlTable").deleteRow(i);
+// }
+
+
+
+x = "<table id='htmlTable' border='1|1'>";
+for (var i = 0; i < arrayI.length; i++) {
+    x+="<tr>";
+    x+="<td>"+arrayI[i]+"</td>";
+    x+="<td>"+ "$" + arrayP[i]+"</td>";
+    // html+="<td>"+rows[i].age+"</td>";
+    x+="<td>"+ "<button onclick = 'removeFromCart()'>Delete</button>" +"</td>";
+
+    x+="</tr>";
 
 }
+
+x+="</table>";
+
+ 
+
+
+display.innerHTML = "<p>Here are the items you are purchasing: </p>" + x + "<p>Total: </p>" + "$" + sumPrices() 
+ + "<p><button onclick='yesOk()'>Purchase</button></p>";
+
+
+// totalSum.innerHTML = "newSum()";//this should only happen on-click of delete button
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //delete if necessary (added 10/26/18):
 
